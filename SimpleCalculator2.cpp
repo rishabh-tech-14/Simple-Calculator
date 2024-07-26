99874f39a2035ba6e969cf3df74f208f71455fbf
Rishabh <rishabhsajwan104@gmail.com>
2024-07-26 18:13:53 +0530

    added new line

--- SimpleCalculator2.cpp
+++ SimpleCalculator2.cpp
@@ -2,16 +2,17 @@
 using namespace std;
 
 int main(){
-    int oper;
-    cout<<"Enter the no. of operation that you want to perform : \n1.'+' \n2.'-' \n3.'*' \n4.'/'"<<endl;
-    cin>>oper;
-
     int num1,num2;
+
     cout<<"enter your first number :";
     cin>>num1;
 
     cout<<"enter your second number :";
     cin>>num2;
+
+    int oper;
+    cout<<"Enter the no. of operation that you want to perform : \n1.'+' \n2.'-' \n3.'*' \n4.'/'"<<endl;
+    cin>>oper;
 
     switch(oper){
 
@@ -26,12 +27,13 @@ int main(){
         case 3:
             cout<<"The Outcome of the Operation is :"<<(num1*num2);
             break;
+        
+        case 4:
+            cout<<"The Outcome of the Operation is :"<<(num1/num2);
 
         default:
-            cout<<"The Outcome of the Operation is :"<<(num1/num2);
+            cout<<"INVALID OPERATION";
             break;
-
-
     }
     return 0;
 }
