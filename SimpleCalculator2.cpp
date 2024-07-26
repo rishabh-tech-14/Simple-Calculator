f02eee189afd334c079ea47a1c19eace47463e0e
Rishabh <rishabhsajwan104@gmail.com>
2024-07-26 17:51:55 +0530

    SIMPLE CALCULATOR

---
+++ SimpleCalculator2.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int oper;
+    cout<<"Enter the no. of operation that you want to perform : \n1.'+' \n2.'-' \n3.'*' \n4.'/'"<<endl;
+    cin>>oper;
+
+    int num1,num2;
+    cout<<"enter your first number :";
+    cin>>num1;
+
+    cout<<"enter your second number :";
+    cin>>num2;
+
+    switch(oper){
+
+        case 1:
+            cout<<"The Outcome of the Operation is :"<<(num1+num2);
+            break;
+
+        case 2:
+            cout<<"The Outcome of the Operation is :"<<(num1-num2);
+            break;
+
+        case 3:
+            cout<<"The Outcome of the Operation is :"<<(num1*num2);
+            break;
+
+        default:
+            cout<<"The Outcome of the Operation is :"<<(num1/num2);
+            break;
+
+
+    }
+    return 0;
+}
